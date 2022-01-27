fbd8736905a30f65efc2ca522183b8f503ac7edc
Vignesh Suresh <vv15@illinois.edu>
2022-01-26 21:15:16 -0600

    two core initial changes

--- include/get_counter.h
+++ include/get_counter.h
@@ -6,8 +6,13 @@
 
 uint64_t get_counter();
 
-void load_aq ();
+void load_aq();
 
-void store_rl ();
+void store_rl();
+
+uint64_t read_hartid();
+#ifdef TWO_CORE_SCHED
+void thread_entry(int cid, int nc);
+#endif
 
 #endif // INCLUDED_GET_COUNTER_H
