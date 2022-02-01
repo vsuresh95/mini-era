0f73dc410d99a0b02e933b4f105c640b0f07b6b2
Vignesh Suresh <vv15@illinois.edu>
2022-02-01 02:10:29 -0600

    helper functions for multicore

--- include/get_counter.h
+++ include/get_counter.h
@@ -6,8 +6,16 @@
 
 uint64_t get_counter();
 
-void load_aq ();
+void load_aq();
 
-void store_rl ();
+void store_rl();
+
+uint64_t read_hartid();
+#ifdef TWO_CORE_SCHED
+void thread_entry(int cid, int nc);
+void amo_add(volatile uint64_t* handshake, uint64_t value);
+uint64_t amo_swap(volatile uint64_t* handshake, uint64_t value);
+void delay(int cycles);
+#endif
 
 #endif // INCLUDED_GET_COUNTER_H
--- src/get_counter.c
+++ src/get_counter.c
@@ -14,14 +14,63 @@ uint64_t get_counter() {
   return counter;
 }
 
-void load_aq () {
+void load_aq() {
 #if(SPANDEX_MODE == 2)
 	asm volatile ("fence r, r");
 #endif
 }
 
-void store_rl () {
+void store_rl() {
 #if(SPANDEX_MODE > 1)
 	asm volatile ("fence w, w");
 #endif
 }
+
+uint64_t read_hartid() {
+	uint64_t hartid;
+
+	asm volatile (
+		"csrr %0, mhartid"
+		: "=r" (hartid)
+	);
+
+	return hartid;
+}	
+
+#ifdef TWO_CORE_SCHED
+void thread_entry(int cid, int nc) {
+	return;
+}
+
+void amo_add(volatile uint64_t* handshake, uint64_t value) {
+ 	asm volatile (
+ 		"mv t0, %1;"
+ 		"mv t2, %0;"
+ 		"amoadd.d.aqrl t1, t0, (t2);"
+ 		:
+ 		: "r" (handshake), "r" (value)
+ 		: "t0", "t1", "t2", "memory"
+ 	);
+}
+
+uint64_t amo_swap(volatile uint64_t* handshake, uint64_t value) {
+ 	uint64_t old_val;
+
+ 	asm volatile (
+ 		"mv t0, %2;"
+ 		"mv t2, %1;"
+ 		"amoswap.d.aqrl t1, t0, (t2);"
+ 		"mv %0, t1"
+ 		: "=r" (old_val)
+ 		: "r" (handshake), "r" (value)
+ 		: "t0", "t1", "t2", "memory"
+ 	);
+ 	return old_val;
+}
+
+void delay(int cycles) {
+ 	for (int i = 0; i < cycles; i++) {
+ 		asm volatile ("nop");
+ 	}
+}
+#endif
